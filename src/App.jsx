d24261d40f1bab41440ce14ca2b65ad58ec34030
PrimeFord <bashfordbash@gmail.com>
2023-03-23 22:10:25 +0100

    feat: Temperature Converter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,45 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [ans, setAns] = useState("");
+
+  const convert = (e) => {
+    e.preventDefault();
+
+    if (from === "celcius") {
+      if (to === "fahrenheit") {
+        s = input * 1.8 + 32;
+      } else if (to === "kelvin") {
+        s = input + 273;
+      } else {
+        s = input * 1;
+      }
+      return s;
+    } else if (from === "fahrenheit") {
+      if (to === "celcius") {
+        s = (input - 32) / 1.8;
+      } else if (to === "kelvin") {
+        s = (input - 32) / 1.8 + 273;
+      } else {
+        s = input * 1;
+      }
+      return s;
+    } else {
+      if (to === "celcius") {
+        s = input - 273;
+      } else if (to === "fahrenheit") {
+        s = (input - 273) * 1.8 + 32;
+      } else {
+        s = input * 1;
+      }
+      return s;
+    }
+    return s;
+    console.log(s);
+  };
 
   return (
     <div className="App">
@@ -16,6 +54,10 @@ function App() {
             <p className="text-[1.25rem] w-[25%] lg:w-fit font-[400]">From:</p>
             <input
               type="number"
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
               className="border-solid border-b-2 lg:w-fit w-[75%]  border-[#e1e1e1] p-1 px-2 focus:outline-none  "
               placeholder="Enter temperature"
             />
@@ -25,27 +67,38 @@ function App() {
               className="first:text-[#e1e1e1] border-solid border-b-2 border-[#3feee6] focus:outline-none first-of-type:text-[#c1c1c1] px-2 py-1"
               name="from"
               id="from"
+              value={from}
+              onChange={(e) => {
+                setFrom(e.target.value);
+              }}
             >
               <option value="null">...Converting from</option>
               <option value="celcius">Celcius</option>
-              <option value="farenheit">Farenheit</option>
+              <option value="fahrenheit">Fahrenheit</option>
               <option value="kelvin">Kelvin</option>
             </select>
             <select
               className="border-solid border-b-2 border-[#3feee6] focus:outline-none px-2 py-2"
               name="from"
               id="from"
+              value={to}
+              onChange={(e) => {
+                setTo(e.target.value);
+              }}
             >
               <option className="first:text-[#c1c1c1]" value="null">
                 ...Converting to
               </option>
               <option value="celcius">Celcius</option>
-              <option value="farenheit">Farenheit</option>
+              <option value="fahrenheit">Fahrenheit</option>
               <option value="kelvin">Kelvin</option>
             </select>
           </section>
           <div className="w-[50%] flex flex-col items-center lg:flex-row gap-4 justify-between mt-4 p-4">
-            <button className="w-[100%] lg:w-[30%] rounded-md bg-[#fc4445] p-2">
+            <button
+              className="w-[100%] lg:w-[30%] rounded-md bg-[#fc4445] p-2"
+              onClick={convert}
+            >
               Calculate
             </button>
             <p className="w-[65%] flex justify-center items-center text-[1.2rem] font-[400]">
@@ -59,3 +112,18 @@ function App() {
 }
 
 export default App;
+
+// const me = (y) => {
+//   if ((y = 2)) {
+//     if (y === 1) {
+//       d = y * 20;
+//     } else {
+//       d = y * 1;
+//     }
+//     return (c = d);
+//   } else {
+//     c = y - 3;
+//   }
+//   return c;
+// };
+// me(2);
